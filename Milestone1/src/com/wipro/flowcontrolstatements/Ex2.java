e57de542439dc26e86dca739e046452f49555940
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-07-28 15:41:53 +0530

    Ex2

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex2.java
@@ -0,0 +1,13 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex2 {
+	public static void main(String[] args) {
+
+		int num=Integer.parseInt(args[0]);
+       if(num%2==0)
+    	   System.out.println("Even");
+       else
+    	   System.out.println("Odd");
+
+	}
+}
