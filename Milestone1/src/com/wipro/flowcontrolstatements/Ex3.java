6b79e2365e7a2f9fe06745cedb1ba6fb01969404
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-07-28 15:54:30 +0530

    Ex3

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex3.java
@@ -0,0 +1,14 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex3 {
+	public static void main(String[] args) {
+		if(args.length == 0)
+			System.out.println("No Values");
+		else
+		{ 
+        System.out.print(args[0]);
+		for(int i=1;i<args.length;i++)
+			System.out.print(","+args[i]);
+		}			
+	}
+}
