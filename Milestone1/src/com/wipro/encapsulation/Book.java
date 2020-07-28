850e161feba80976712bdf253759f03ca1d88a86
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-07-28 16:52:35 +0530

    Book

---
+++ Milestone1/src/com/wipro/encapsulation/Book.java
@@ -0,0 +1,38 @@
+package com.wipro.encapsulation;
+
+public class Book {
+	 private String name;
+	   private Author author;
+	   private double price;
+	   private int qtyInstock;
+	   public Book(String name, Author author, double price, int qtyInstock) {
+	      this.name = name;
+	      this.author = author;
+	      this.price = price;
+	      this.qtyInstock = qtyInstock;
+	   }
+	 
+	   public String getName() {
+	      return name;
+	   }
+	   public Author getAuthor() {
+	      return author;  
+	   }
+	   
+	   public double getPrice() {
+	      return price;
+	   }
+	   public void setPrice(double price) {
+	      this.price = price;
+	   }
+	   public int getQtyInstock() {
+	      return qtyInstock;
+	   }
+	   public void setQtyInstock(int qtyInstock) {
+	      this.qtyInstock = qtyInstock;
+	   }
+	 
+	   public String toString() {
+	      return "'" + name + "' by " + author; 
+	   }
+}
