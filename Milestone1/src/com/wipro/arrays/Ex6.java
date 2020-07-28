57318e80480cf3935ae89d6297b920b24344f48f
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-07-28 16:45:34 +0530

    Ex6

---
+++ Milestone1/src/com/wipro/arrays/Ex6.java
@@ -0,0 +1,26 @@
+package com.wipro.arrays;
+
+public class Ex6 {
+	public static void main(String[] args) {
+
+		 int arr[] ={6, 9, 3, 5, 4};     
+	        int temp = 0;    
+	        
+	        for (int i = 0; i < arr.length; i++) {     
+	            for (int j = i+1; j < arr.length; j++) {     
+	               if(arr[i] > arr[j]) {    
+	                   temp = arr[i];    
+	                   arr[i] = arr[j];    
+	                   arr[j] = temp;    
+	               }     
+	            }     
+	        }    
+	          
+	        System.out.println("Elements of array sorted order: ");    
+	        for (int i = 0; i < arr.length; i++) {     
+	            System.out.print(arr[i] + " ");    
+	        }    
+
+	}
+
+}
