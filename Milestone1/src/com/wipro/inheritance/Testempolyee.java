3de4b044cc2fb8c08249dd8a82d676f452e3813d
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-07-28 16:55:25 +0530

    Testemployee

---
+++ Milestone1/src/com/wipro/inheritance/Testempolyee.java
@@ -0,0 +1,11 @@
+package com.wipro.inheritance;
+
+public class Testempolyee {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p=new Person("kandipati");
+		Employee e=new Employee("yaswanth",10000,2015,"abcd");
+		System.out.println(p);
+		System.out.println(e);
+	}
+}
