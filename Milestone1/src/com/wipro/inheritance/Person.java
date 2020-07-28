4aea90183b77ad8c1580e82999ccfa724a24cc85
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-07-28 16:54:56 +0530

    Person

---
+++ Milestone1/src/com/wipro/inheritance/Person.java
@@ -0,0 +1,18 @@
+package com.wipro.inheritance;
+
+public class Person {
+	private String name;
+	public Person(String n) {
+		name=n;
+	}
+	public void setName(String n) {
+		name=n;
+	}
+	public String getName() {
+		return name;
+	}
+	public String toString() {
+		return name;
+	}
+
+}
