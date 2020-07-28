85a5bcb2af5290f8d9380c209e4212f3a36b4d63
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-07-28 15:08:34 +0530

    Ex1

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex1.java
@@ -0,0 +1,14 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex1 {
+	public static void main(String[] args) {
+
+		int num=Integer.parseInt(args[0]);
+		if(num>0)
+			System.out.println("Positive");
+		else if(num<0)
+			System.out.println("Negative");
+		else
+			System.out.println("Zero");
+			}
+}
