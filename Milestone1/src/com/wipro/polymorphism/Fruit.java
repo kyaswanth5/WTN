045e92f10a0c0367ecfed16da12f2434151de164
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-07-29 16:00:15 +0530

    Fruit

---
+++ Milestone1/src/com/wipro/polymorphism/Fruit.java
@@ -0,0 +1,33 @@
+package com.wipro.polymorphism;
+
+public class Fruit {
+	char name,taste,size;
+
+	  public static void main(String args[])
+	  {
+	    Apple a=new Apple();
+	    a.eat();
+	    Orange o=new Orange();
+	    o.eat();
+	  }
+	  void eat()
+	  {
+	    System.out.println("name:Banana");
+	    System.out.println("taste:sweet");
+	  }
+	}
+	class Apple extends Fruit{
+	  void eat()
+	  {
+	    System.out.println("name:Orange");
+	    System.out.println("taste:sweeet");
+	  }
+	}
+	class Orange extends Fruit{
+	  void eat()
+	  {
+	    System.out.println("name:Apple");
+	    System.out.println("taste:sour");
+	  }
+
+}
