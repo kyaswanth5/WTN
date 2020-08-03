8786f1fbe34f64223d2ff0caf04e308bcf4178b6
kyaswanth5 <66764934+kyaswanth5@users.noreply.github.com>
2020-08-03 15:59:44 +0530

    Add files via upload

---
+++ Milestone2/src/com/wipro/Ex1.java
@@ -0,0 +1,18 @@
+package com.wipro.maps;
+import java.util.HashMap;
+public class Ex1 {
+	public static void main(String[] args) {
+		CountryMap countryMap = new CountryMap();
+
+		countryMap.saveCountryCapital("India", "Delhi");
+		countryMap.saveCountryCapital("Japan", "Tokyo");
+		countryMap.saveCountryCapital("USA", "Washington, D.C.");
+
+		System.out.println(countryMap.getCapital("India"));
+		System.out.println(countryMap.getCountry("Tokyo"));
+		System.out.println(countryMap.toArrayList());
+
+		HashMap<String, String> M2 = countryMap.swapKyeValue();
+		System.out.println(M2);
+		}
+}
